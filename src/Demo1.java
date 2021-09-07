4a287bd9ec6d3126dbbabb815fc3b96d3722dd85
yusu <yusu_liu@outlook.com>
2021-09-07 21:08:21 +0800

    新建demo类，输出hello git --rain

---
+++ src/Demo1.java
@@ -0,0 +1,17 @@
+/**
+ * @Author: rain
+ * @Project: TestProject
+ * @Version: V1.0
+ * @Description: PACKAGE_NAME
+ * @Date: 2021/9/7 21:06
+ * @Goal: 从零开始，以终为始
+ */
+public class Demo1 {
+    /**
+     * 测试demo类
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
